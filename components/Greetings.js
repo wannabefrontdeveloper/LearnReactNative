329d5e2b49ee7879ff4ace68dba15615c50a25bb
wannabefrontdeveloper <giwonk@naver.com>
2024-02-06 19:03:31 +0900

    컴포넌트 생성 함수 컴포넌트를 생성하였습니다.

---
+++ components/Greetings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Greeting() {
+  return (
+    <View>
+      <Text>안녕하세요 함수 컴포넌트!</Text>
+    </View>
+  );
+}
+
+export default Greeting;
