d696f6ffe595e93ef040b8c4c894dfe4436edab2
wannabefrontdeveloper <giwonk@naver.com>
2024-02-06 19:38:05 +0900

    코드수정 View 아래에 새로운 Text 컴포넌트를 넣어주었습니다.

--- components/Greetings.js
+++ components/Greetings.js
@@ -4,7 +4,10 @@ import {View, Text} from 'react-native';
 function Greeting(props) {
   return (
     <View>
-      <Text>안녕하세요 {props.name}</Text>
+      <View>
+        <Text>안녕하세요 {props.name}</Text>
+      </View>
+      <Text>Extra Text!</Text>
     </View>
   );
 }
