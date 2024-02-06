0fa919590aab490f9a3decbde38fa5aac815d527
wannabefrontdeveloper <giwonk@naver.com>
2024-02-06 20:34:48 +0900

    코드수정 App 컴포넌트에서 Counter 컴포넌트에 전달해야 할 상태를 관리해주었습니다.

--- App.js
+++ App.js
@@ -3,9 +3,13 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 import Counter from './components/Counter';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => setCount(count + 1);
+  const onDecrease = () => setCount(count - 1);
   return (
     <SafeAreaView style={styles.full}>
-      <Counter />
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
