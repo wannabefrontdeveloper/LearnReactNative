a8524f16eada4a1432039515e1c55d69e53fab6d
wannabefrontdeveloper <giwonk@naver.com>
2024-02-06 19:47:17 +0900

    코드수정 Box 컴포넌트를 사용하도록 하였습니다.

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import Greeting from './components/Greetings';
+import Box from './components/Box';
 
 const App = () => {
-  const name = 'JSX';
   return (
     <SafeAreaView>
-      <Greeting name={name} />
+      <Box />
     </SafeAreaView>
   );
 };
