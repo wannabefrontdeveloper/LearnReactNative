40297dec929eb9270c4ae20975cd30c049d3715c
wannabefrontdeveloper <giwonk@naver.com>
2024-02-06 20:17:32 +0900

    코드수정 SafeAreaView가 전체 영역을 사용하게끔 설정하였습니다.

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import React, {useState} from 'react';
-import {SafeAreaView, Button} from 'react-native';
-import Box from './components/Box';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
-  const onPress = () => {
-    setVisible(!visible);
-  };
-  return (
-    <SafeAreaView>
-      <Button title="토글" onPress={onPress} />
-      {visible ? <Box rounded={true} size="large" color="blue" /> : null}
-    </SafeAreaView>
-  );
+  return <SafeAreaView style={styles.full}></SafeAreaView>;
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+  },
+});
 
 export default App;
