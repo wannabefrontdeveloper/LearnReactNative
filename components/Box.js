87f5e1fcdfc0570337a34f30d8c999b331ae9e9c
wannabefrontdeveloper <giwonk@naver.com>
2024-02-06 19:59:37 +0900

    코드추가 small, medium, large 스타일을 준비한 다음 이 스타일들을 sizes 라는 객체에 넣어주었습니다.

--- components/Box.js
+++ components/Box.js
@@ -2,8 +2,16 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
 function Box(props) {
-  return <View style={[styles.box, props.rounded ? styles.rounded : null]} />;
+  return (
+    <View
+      style={[styles.box, props.rounded && styles.rounded, sizes[props.size]]}
+    />
+  );
 }
+
+Box.defaultProps = {
+  size: 'medium',
+};
 
 const styles = StyleSheet.create({
   box: {
@@ -14,6 +22,24 @@ const styles = StyleSheet.create({
   rounded: {
     borderRadius: 16,
   },
+  small: {
+    width: 32,
+    height: 32,
+  },
+  medium: {
+    width: 64,
+    height: 64,
+  },
+  large: {
+    width: 128,
+    height: 128,
+  },
 });
+
+const sizes = {
+  small: styles.small,
+  medium: styles.medium,
+  large: styles.large,
+};
 
 export default Box;
