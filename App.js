d90aee163f8372d6cfc5f07ac82898e5fa35b470
wannabefrontdeveloper <giwonk@naver.com>
2024-02-06 20:12:47 +0900

    코드수정 Button 컴포넌트를 사용하도록 하였습니다.

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, {useState} from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Button} from 'react-native';
 import Box from './components/Box';
 
 const App = () => {
   const [visible, setVisible] = useState(true);
+  const onPress = () => {
+    setVisible(!visible);
+  };
   return (
     <SafeAreaView>
+      <Button title="토글" onPress={onPress} />
       <Box rounded={true} size="large" color="blue" />
     </SafeAreaView>
   );
