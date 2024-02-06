bdfb72b3c5d91b4be66fe12d701cc9ab54365ea4
wannabefrontdeveloper <giwonk@naver.com>
2024-02-06 20:10:26 +0900

    코드수정 useState를 사용하도록 하였고, visible과 setVisible을 선언하였습니다.

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native';
 import Box from './components/Box';
 
 const App = () => {
+  const [visible, setVisible] = useState(true);
   return (
     <SafeAreaView>
       <Box rounded={true} size="large" color="blue" />
